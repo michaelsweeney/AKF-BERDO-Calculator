09a24790a970c49a318585b675f91c5b574ae70a
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2021-12-22 12:37:17 -0500

    d

--- src/components/userinputs/numberinput.js
+++ src/components/userinputs/numberinput.js
@@ -1,7 +1,7 @@
 import TextField from "@mui/material/TextField";
 import { makeStyles } from "@material-ui/styles";
 import NumberFormat from "react-number-format";
-import * as d3 from "d3";
+
 const useStyles = makeStyles({
   root: {},
   label: { marginBottom: 5, color: "gray" },
@@ -25,15 +25,6 @@ const NumberInput = (props) => {
         thousandSeparator=","
         sx={{ width: "80%" }}
       />
-
-      {/* <TextField
-        sx={{ width: "100%" }}
-        onChange={changeCallback}
-        type="number"
-        value={value}
-        variant="standard"
-        inputProps={{ step: step }}
-      /> */}
     </div>
   );
 };
