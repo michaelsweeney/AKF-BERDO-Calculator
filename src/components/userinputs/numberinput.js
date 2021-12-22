aa1e40a30996d6aaa7eb39e35257052d43949df6
Michael Sweeney <michael.samuel.sweeney@gmail.com>
2021-12-22 12:36:53 -0500

    d

--- src/components/userinputs/numberinput.js
+++ src/components/userinputs/numberinput.js
@@ -11,10 +11,6 @@ const useStyles = makeStyles({
 const NumberInput = (props) => {
   const { label, changeCallback, value } = props;
   const classes = useStyles();
-  const step = 500;
-
-  const formatValue = d3.format(",")(value);
-  console.log(formatValue);
 
   return (
     <div>
@@ -23,9 +19,11 @@ const NumberInput = (props) => {
       <NumberFormat
         value={value}
         customInput={TextField}
+        variant="standard"
         type="text"
         onValueChange={(e) => (e ? changeCallback(e.value) : changeCallback(e))}
         thousandSeparator=","
+        sx={{ width: "80%" }}
       />
 
       {/* <TextField
